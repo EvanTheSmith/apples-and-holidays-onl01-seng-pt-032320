1f028f99cdd22cebaced360f440b4718a5542af5
Evan Smith <professionalemail@zoho.com>
2020-04-05 21:52:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -99,8 +99,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.each do |season,two|
-    binding.pry
+  holiday_hash.each do |season,holiday|
+    holiday.each do |day, activities|
+      binding.pry
+    end
   end
   
 end
